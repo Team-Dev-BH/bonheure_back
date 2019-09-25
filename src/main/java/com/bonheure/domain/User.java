76311e1fa499056c1d156ac504c27cfd073e1c42
BorhenCherif <borhenenib@gmail.com>
2019-09-25 14:43:50 +0200

    [TA-02]user and role entity creation H2 database configuration

---
+++ src/main/java/com/bonheure/domain/User.java
@@ -0,0 +1,156 @@
+package com.bonheure.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(indexes = {
+		@Index(name = "user_reference",columnList = "reference",unique = true)
+})
+public class User {
+	
+	@Id
+	private long id;
+	
+	@Column(unique = true)
+	private String reference;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private int mobileNumber;
+	
+	private String password;
+	
+	private LocalDateTime creationDate;
+	
+	private LocalDateTime modificationDate;
+	
+	private LocalDateTime activationDate;
+	
+	private Boolean activated;
+	
+	@ManyToOne
+	private Role role;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+
+	public int getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(int mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDateTime getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDateTime creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public LocalDateTime getModificationDate() {
+		return modificationDate;
+	}
+
+	public void setModificationDate(LocalDateTime modificationDate) {
+		this.modificationDate = modificationDate;
+	}
+
+	public LocalDateTime getActivationDate() {
+		return activationDate;
+	}
+
+	public void setActivationDate(LocalDateTime activationDate) {
+		this.activationDate = activationDate;
+	}
+
+	public Boolean getActivated() {
+		return activated;
+	}
+
+	public void setActivated(Boolean activated) {
+		this.activated = activated;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}

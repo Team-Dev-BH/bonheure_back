a989569b845cf337a753dfeb3ef0833995c2814c
BorhenCherif <borhenenib@gmail.com>
2019-10-07 16:19:41 +0200

    Working	Area CRUD

---
+++ src/main/java/com/bonheure/controller/dto/WorkingAreaDTO.java
@@ -0,0 +1,76 @@
+package com.bonheure.controller.dto;
+
+public class WorkingAreaDTO  {
+	
+	
+	 private String reference;
+
+	 private Double latitude;
+
+	 private Double longitude;
+	 
+	 private String region;
+
+	 
+
+	 /**
+	 * @return the reference
+	 */
+	public String getReference() {
+		return reference;
+	}
+
+	/**
+	 * @param reference the reference to set
+	 */
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	/**
+	 * @return the latitude
+	 */
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	/**
+	 * @param latitude the latitude to set
+	 */
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	/**
+	 * @return the longitude
+	 */
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	/**
+	 * @param longitude the longitude to set
+	 */
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	/**
+	 * @return the region
+	 */
+	public String getRegion() {
+		return region;
+	}
+
+	/**
+	 * @param region the region to set
+	 */
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+
+
+	 
+	 
+}
